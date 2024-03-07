217a22324defb23fd1c42a5916f933613c29914b
Xiao Sun <xsun11@ualberta.ca>
2024-03-07 15:48:58 -0700

    send_notification1

--- code/app/src/main/java/com/example/eventlink_qr_pro/SendNotificationActivity.java
+++ code/app/src/main/java/com/example/eventlink_qr_pro/SendNotificationActivity.java
@@ -1,18 +1,54 @@
 package com.example.eventlink_qr_pro;
 
-import android.os.Bundle;
-import android.widget.Button;
+import static android.content.ContentValues.TAG;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class SendNotificationActivity extends AppCompatActivity {
     Button cancelButton;
+
+    EditText etToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_notification); // The layout file for sending notifications
+        etToken = findViewById(R.id.description);
 
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed");
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+
+                        System.out.println(token);
+                        Toast.makeText(SendNotificationActivity.this, "Your device registration token is " + token
+                                , Toast.LENGTH_SHORT).show();
+
+                        etToken.setText(token);
+
+                    }
+                });
         cancelButton = findViewById(R.id.cancel);
 
         cancelButton.setOnClickListener(view -> {
