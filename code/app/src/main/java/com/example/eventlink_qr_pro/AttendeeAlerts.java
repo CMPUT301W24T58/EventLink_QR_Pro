d18fda83b69dbf8c277343882e4a8c36ee13807d
Xiao Sun <xsun11@ualberta.ca>
2024-03-08 02:35:23 -0700

    update with attendee alerts

---
+++ code/app/src/main/java/com/example/eventlink_qr_pro/AttendeeAlerts.java
@@ -0,0 +1,15 @@
+package com.example.eventlink_qr_pro;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AttendeeAlerts extends AppCompatActivity{
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.attendee_alerts);
+        }
+}
