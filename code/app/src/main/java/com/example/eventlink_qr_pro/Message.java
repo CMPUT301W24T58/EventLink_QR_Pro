e5a2760de859341e0b0fb1fea088465bdf789bc2
Xiao Sun <xsun11@ualberta.ca>
2024-03-28 17:33:30 -0600

    notification

---
+++ code/app/src/main/java/com/example/eventlink_qr_pro/Message.java
@@ -0,0 +1,26 @@
+package com.example.eventlink_qr_pro;
+
+public class Message {
+    private String title;
+    private String description;
+
+    public Message() {
+
+    }
+
+    public Message(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    
+}
+
--- code/app/src/main/java/com/example/eventlink_qr_pro/SendNotificationActivity.java
+++ code/app/src/main/java/com/example/eventlink_qr_pro/SendNotificationActivity.java
@@ -13,19 +13,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class SendNotificationActivity extends AppCompatActivity {
-    Button cancelButton;
-
-    EditText etToken;
+    Button cancelButton, pushButton;
+    EditText etToken, etMessageTitle, etMessageDescription;
+    String eventName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.send_notification); // The layout file for sending notifications
+        setContentView(R.layout.send_notification);
         etToken = findViewById(R.id.description);
+        eventName = getIntent().getStringExtra("eventName");
+        etMessageTitle = findViewById(R.id.notificationTitle);
+        etMessageDescription = findViewById(R.id.description);
+        cancelButton = findViewById(R.id.cancel);
+        pushButton = findViewById(R.id.push);
 
+        cancelButton.setOnClickListener(view -> {
+            finish();
+        });
+
+        pushButton.setOnClickListener(view -> {
+            String messageTitle = etMessageTitle.getText().toString();
+            String messageDescription = etMessageDescription.getText().toString();
+            pushNotification(eventName, messageTitle, messageDescription);
+        });
 
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(new OnCompleteListener<String>() {
@@ -39,22 +54,42 @@ public class SendNotificationActivity extends AppCompatActivity {
                         // Get new FCM registration token
                         String token = task.getResult();
 
-                        // Log and toast
-
-                        System.out.println(token);
-                        Toast.makeText(SendNotificationActivity.this, "Your device registration token is " + token
-                                , Toast.LENGTH_SHORT).show();
-
-                        etToken.setText(token);
 
                     }
                 });
-        cancelButton = findViewById(R.id.cancel);
+    }
 
-        cancelButton.setOnClickListener(view -> {
-            // Close this activity and go back to the previous one
-            finish();
-        });
+    private void pushNotification(String eventName, String messageTitle, String messageDescription) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        NotificationMessage message = new NotificationMessage(messageTitle, messageDescription);
+        db.collection("/events").document(eventName).collection("messages")
+                .add(message)
+                .addOnSuccessListener(documentReference -> {
+                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    Toast.makeText(this, "Notification pushed successfully", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    Log.w(TAG, "Error adding document", e);
+                    Toast.makeText(this, "Error pushing notification", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    static class NotificationMessage {
+        String title, description;
+
+        public NotificationMessage(String title, String description) {
+            this.title = title;
+            this.description = description;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
     }
 }
 
