741ad59cb6331199eabcd392a25305a94cc0bff3
cmcgilve <cmcgilve@ualberta.ca>
2024-04-03 17:51:52 -0600

    (Maybe) push notifications when receiving messages from firebase Messaging

--- code/app/src/main/java/com/example/eventlink_qr_pro/MyFirebaseMessagingService.java
+++ code/app/src/main/java/com/example/eventlink_qr_pro/MyFirebaseMessagingService.java
@@ -1,11 +1,16 @@
 package com.example.eventlink_qr_pro;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        String title = remoteMessage.getNotification().getTitle();
+        String content = remoteMessage.getNotification().getBody();
+        NotificationHelper.buildNotification(title, content, this);
     }
 }
