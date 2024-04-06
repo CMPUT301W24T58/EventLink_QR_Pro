3372e8d254ac90be1f8cfad4b05e470a84ecfb31
Shuran Kong <skong1@ualberta.ca>
2024-04-06 12:49:44 -0600

    JAVADOC
    MyFirebaseMessagingService&NotificationHelper&OrganizerAlerts

--- code/app/src/main/java/com/example/eventlink_qr_pro/OrganizerAlerts.java
+++ code/app/src/main/java/com/example/eventlink_qr_pro/OrganizerAlerts.java
@@ -16,7 +16,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * An activity that displays a list of alerts for organizers based on specific milestones reached within an event,
+ * such as a certain number of attendees registering. The activity listens for real-time updates to milestone data
+ * stored in Firestore and updates the UI accordingly.
+ */
 public class OrganizerAlerts extends AppCompatActivity {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -25,7 +29,13 @@ public class OrganizerAlerts extends AppCompatActivity {
     private List<Alert> alertsList;
     private String eventName;
     private Button back;
-
+    /**
+     * Initializes the activity by setting the content view, retrieving and setting up the ListView for displaying
+     * alerts, and fetching the event name from the intent. If the event name is not provided, the activity will display
+     * a toast message and finish. It also sets up a listener to fetch and display milestone alerts from Firestore.
+     *
+     * @param savedInstanceState Contains data of the activity's previously saved state. It's null the first time the activity is created.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,10 +66,15 @@ public class OrganizerAlerts extends AppCompatActivity {
         });
     }
 
-
+    /**
+     * Sets up a real-time listener for the specified event's milestones collection in Firestore. For each milestone document,
+     * it constructs an alert with a message indicating the milestone reached and the time it was reached, then updates the UI.
+     *
+     * @param eventName The name of the event for which milestones are being monitored.
+     */
     private void setupMilestoneListener(String eventName) {
         db.collection("events").document(eventName).collection("milestones")
-                .orderBy("timestamp") // Assuming you have a timestamp field to order by
+                .orderBy("timestamp") 
                 .addSnapshotListener((queryDocumentSnapshots, error) -> {
                     if (error != null) {
                         Toast.makeText(OrganizerAlerts.this, "Error loading milestones.", Toast.LENGTH_SHORT).show();
